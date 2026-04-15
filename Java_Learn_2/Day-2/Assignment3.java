abe6f1d8a8c333f8daa8d640839c92484aa25208
Sauvik04 <sauviknandi2004@gmail.com>
2026-04-15 16:24:21 +0530

    Added Assignment5

--- Java_Learn_2/Day-2/Assignment3.java
+++ Java_Learn_2/Day-2/Assignment3.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Assinment3 {
+public class Assignment3 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter N: ");
