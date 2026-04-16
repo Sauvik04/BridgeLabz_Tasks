819b96529c19f2a324a4c59d19eb340e9a7eee4a
Sauvik04 <sauviknandi2004@gmail.com>
2026-04-16 15:28:00 +0530

    Added Day-1 Assignment 5

---
+++ Java_Learn_2/Day-1/Day1Assignment5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.*;
+public class Day1Assignment5 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No name provided");
+        } else {
+            System.out.println("Hello, " + args[0]);
+        }
+    }
+}
