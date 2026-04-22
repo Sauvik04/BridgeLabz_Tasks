8c679aab97663473f5430b8f04b75b0eac18c6c9
Sauvik04 <sauviknandi2004@gmail.com>
2026-04-22 12:10:26 +0530

    Add UC2: Add new contact

--- Java_Learn_2/Day-9/Day9Assignment1.java
+++ Java_Learn_2/Day-9/Day9Assignment1.java
@@ -1,16 +1,32 @@
 import java.util.*;
+
 class Contact {
-    String firstName;
-    String lastName;
-    String address;
-    String city;
-    String state;
+    String firstName, lastName, address, city, state;
     int zip;
-    String phone;
-    String email;
+    String phone, email;
 }
+
 public class Day9Assignment1 {
-    public static void main(String[]args){
-        System.out.println("Welcome to Address Book Program");
+    static ArrayList<Contact> list = new ArrayList<>();
+    public static void addContact() {
+        Scanner sc = new Scanner(System.in);
+
+        Contact c = new Contact();
+
+        System.out.print("First Name: ");
+        c.firstName = sc.nextLine();
+
+        System.out.print("Last Name: ");
+        c.lastName = sc.nextLine();
+
+        System.out.print("City: ");
+        c.city = sc.nextLine();
+
+        list.add(c);
+
+        System.out.println("Contact added!");
+    }
+    public static void main(String[] args) {
+        addContact();
     }
 }
