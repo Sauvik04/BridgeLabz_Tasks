ef2d81bde5a82a4ce19714b0a5e2ec2b9badde94
Sauvik04 <sauviknandi2004@gmail.com>
2026-04-22 12:36:47 +0530

    Add UC3: Edit contact

--- Java_Learn_2/Day-9/Day9Assignment1.java
+++ Java_Learn_2/Day-9/Day9Assignment1.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Contact {
     String firstName, lastName, address, city, state;
     int zip;
@@ -26,7 +25,24 @@ public class Day9Assignment1 {
 
         System.out.println("Contact added!");
     }
+    public static void editContact() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First Name to Edit: ");
+        String name = sc.nextLine();
+
+        for (Contact c : list) {
+            if (c.firstName.equals(name)) {
+                System.out.print("Enter New City: ");
+                c.city = sc.nextLine();
+                System.out.println("Contact updated!");
+                return;
+            }
+        }
+        System.out.println("Contact not found!");
+    }
     public static void main(String[] args) {
         addContact();
+        editContact();
     }
 }
