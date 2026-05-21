9cc1ca0127572ca8f70ed3c718e04ef9318975b9
Sauvik04 <sauviknandi2004@gmail.com>
2026-05-21 12:42:11 +0530

    UC1 Added hotels

--- Java_Learn_2/Day-25/Hotel.java
+++ Java_Learn_2/Day-25/Hotel.java
@@ -1,2 +1,15 @@
 public class Hotel {
+
+    String name;
+    int rate;
+
+    public Hotel(
+            String name,
+            int rate){
+
+        this.name=name;
+        this.rate=rate;
+
+    }
+
 }
